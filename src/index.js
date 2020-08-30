a3a7004af5d481c1c2689a9f5c2bfa2bb1568b59
Dumenskii Daniil <dumenskii.daniil@gmail.com>
2020-08-30 15:57:48 +0300

    Настроил конфигурацию Webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "@style/style";
+import data from "./assets/data.json";
+
+console.log(123);
+console.log(data);
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserWebpaclPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+const DEV = process.env.NODE_ENV === "development";
+
+let filename = (ext) => (DEV ? `[name].${ext}` : `[contenthash].${ext}`);
+
+let opt = () => {
+  let conf = {};
+  if (!DEV) {
+    conf.minimizer = [new TerserWebpaclPlugin(), new OptimizeCssAssetsWebpackPlugin()];
+  }
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    index: "./index.js",
+  },
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: opt(),
+  resolve: {
+    extensions: [".js", ".json", ".scss"],
+    alias: {
+      "@style": path.resolve(__dirname, "src/scss"),
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+  ],
+  devServer: {
+    port: 1234,
+    hot: DEV,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: DEV,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
