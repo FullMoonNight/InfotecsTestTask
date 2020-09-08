6d979d708bd3558a14234039fcc5fece2d388e03
Dumenskii Daniil <dumenskii.daniil@gmail.com>
2020-09-08 20:42:47 +0300

    оставил только папку dist

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import tableGeneration from "@scripts/renderTable";
-import * as hide from "@scripts/hideColumn";
-import * as change from "@scripts/changeCard";
-import "@style/style";
-
-tableGeneration();
